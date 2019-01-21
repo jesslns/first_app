ce1653d1a4bcb09e10add0518f58e248714629a5
jesslns <jesslns@yahoo.com.hk>
2019-01-21 17:25:41 +0000

    Made cat name dynamic

--- app.rb
+++ app.rb
@@ -15,7 +15,13 @@ get '/shotgun' do
   'Life is less painful with shotgun gem :)'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
