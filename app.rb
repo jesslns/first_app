5ff467ef48bf53786a460726824601f6d26dd310
jesslns <jesslns@yahoo.com.hk>
2019-01-21 15:45:24 +0000

    Use shotgun gem and add a few routes

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'shotgun'
 
 get '/' do
   'Hello world!'
+end
+
+get '/secret' do
+  'This is a secret...'
+end
+
+get '/shotgun' do
+  'Life is less painful with shotgun gem :)'
 end
